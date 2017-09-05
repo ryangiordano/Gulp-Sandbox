6330ae6d295530e2a10ddc04bc301e0f8a306559
RyanGiordano <ryan@gocodigo.com>
2017-09-05 13:53:47 -0400

    made project generic

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,2 @@
-"use strict";
-
 /* Website Produced by Codigo |  gocodigo.com */
-var KioskInstance = new Kiosk({ project_name: "Codigo Kiosk" });
-KioskInstance.init();
-
-$(function () {
-    FastClick.attach(document.body);
-});
+"use strict";
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
 /* Website Produced by Codigo |  gocodigo.com */
-const KioskInstance = new Kiosk({project_name:"Codigo Kiosk"});
-KioskInstance.init();
-
-$(function() {
-    FastClick.attach(document.body);
-});
